412940c39b7488ca3cc8c25b19c73dcee44d99f0
romanyukalexandr84 <romanyukalexandr84@gmail.com>
2023-06-07 07:10:09 +0300

    add task2

--- TasksForHomework1.java
+++ TasksForHomework1.java
@@ -4,6 +4,13 @@ public class TasksForHomework1 {
     public static void main(String[] args) {
         Random random = new Random();
         int i = random.nextInt(2000);
-        System.out.println("Случайное целое число = "+i);
+        System.out.println("Случайное целое число = " + i);
+
+        int n = 1 << 11;   //двойку в нулевой степени смещаем побитово влево, 
+                           //пока не превысим 2000 (на 11 разрядов)
+        while (i < n)
+            n >>= 1;       //смещаемся побитово вправо, 
+                           //пока не превысим выпавшее число i
+        System.out.println("Номер старшего значащего бита = " + n);
     }
 }
