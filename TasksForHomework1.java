b924d8d38a6e8039f0433f4bd88ae98de5c027a9
romanyukalexandr84 <romanyukalexandr84@gmail.com>
2023-06-07 10:41:42 +0300

    add task4

--- TasksForHomework1.java
+++ TasksForHomework1.java
@@ -2,9 +2,9 @@ import java.util.Random;
 
 public class TasksForHomework1 {
 
-    public static void PrintArray(short[] arr) {
-        for (short l = 0; l < arr.length; l++) {
-            System.out.print(arr[l] + " ");
+    public static void PrintArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
@@ -12,27 +12,27 @@ public class TasksForHomework1 {
     public static void main(String[] args) {
 
         Random random = new Random();
-        short i = (short) random.nextInt(2000);
+        int i = random.nextInt(2000);
         System.out.println("Случайное целое число (i) = " + i);
 
 
-        short n = 1 << 11; // двойку в нулевой степени смещаем побитово влево,
-                           // пока не превысим 2000 (на 11 разрядов)
+        int n = 1 << 11; // двойку в нулевой степени смещаем побитово влево,
+                         // пока не превысим 2000 (на 11 разрядов)
         while (i < n)
-            n >>= 1; // смещаемся побитово вправо,
-                     // пока не превысим выпавшее число i
+            n >>= 1;     // смещаемся побитово вправо,
+                         // пока не превысим выпавшее число i
         System.out.println("Номер старшего значащего бита (n) = " + n);
 
-        
-        short count = 0;
-        for (short j = i; j < Short.MAX_VALUE; j++) {
+
+        int count = 0;
+        for (int j = i; j < Short.MAX_VALUE; j++) {
             if (j % n == 0) {
                 count++;
             }
         }
-        short m1[] = new short[count];
-        short k = 0;
-        for (short j = i; j < Short.MAX_VALUE; j++) {
+        int m1[] = new int[count];
+        int k = 0;
+        for (int j = i; j < Short.MAX_VALUE; j++) {
             if (j % n == 0) {
                 m1[k] = j;
                 k++;
@@ -40,5 +40,23 @@ public class TasksForHomework1 {
         }
         System.out.println("Все кратные n числа в диапазоне от i до Short.MAX_VALUE:");
         PrintArray(m1);
+
+
+        int count2 = 0;
+        for (int j = Short.MIN_VALUE; j < i; j++) {
+            if (j % n != 0) {
+                count2++;
+            }
+        }
+        int m2[] = new int[count2];
+        k = 0;
+        for (int j = Short.MIN_VALUE; j < i; j++) {
+            if (j % n != 0) {
+                m2[k] = j;
+                k++;
+            }
+        }
+        System.out.println("Все некратные n числа в диапазоне от Short.MIN_VALUE до i:");
+        PrintArray(m2);
     }
 }
