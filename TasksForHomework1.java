1b99e4662452da3c1f133b8dbe5d7c2cb4d88ddd
romanyukalexandr84 <romanyukalexandr84@gmail.com>
2023-06-07 06:24:33 +0300

    add task1

---
+++ TasksForHomework1.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class TasksForHomework1 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int i = random.nextInt(2000);
+        System.out.println("Случайное целое число = "+i);
+    }
+}
